50663a35cf02cad01a109898c1f2958738ab434f
Alexey Agapitov <alexey.agapitov@zoho.com>
2015-11-25 23:10:50 +0300

    NetworkHelper - no instances.

--- app/src/main/java/com/marwinxxii/reactiveui/network/NetworkHelper.java
+++ app/src/main/java/com/marwinxxii/reactiveui/network/NetworkHelper.java
@@ -1,6 +1,10 @@
 package com.marwinxxii.reactiveui.network;
 
 public final class NetworkHelper {
+    private NetworkHelper() {
+        throw new AssertionError("No instances");
+    }
+
     public static RealtyApi provideApi() {
         return new ApiStub();
     }
